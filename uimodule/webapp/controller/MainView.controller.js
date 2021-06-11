bc905699c3f9f51bd43f29ba7e03ec35045b5c4f
t2ga <tltsnsub@gmail.com>
2021-06-11 11:13:54 +0300

    Initialize repository with easy-ui5

---
+++ uimodule/webapp/controller/MainView.controller.js
@@ -0,0 +1,5 @@
+sap.ui.define(["com/zwork/dabin/controller/BaseController"], function (Controller) {
+    "use strict";
+
+    return Controller.extend("com.zwork.dabin.controller.MainView", {});
+});
